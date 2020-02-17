b7f32d760fbaf0dac8934329de88a0181fd58e65
LJKing8 <Laura.j.king8@gmail.com>
2020-02-17 10:45:23 -0800

    BEM module based css setup, banner styled,webpack-dev-server configured

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,30 @@
 // webpack bundles data to help loading times and keeps everything orginized
 const path = require('path');
 const postCSSPlugins = [
+    require('postcss-import'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
     require('autoprefixer')
-]
+];
+
 module.exports = {
     entry: './app/assets/scripts/App.js',// what we want to bundle
     output: {
         filename: 'bundled.js',
         path: path.resolve(__dirname, 'app')
     },// where we want to put the bundeled code
+    devServer: {
+        before: function(app, server) {
+            server._watch('./app/**/*.html');
+        },
+        contentBase: path.join(__dirname, 'app'),
+        hot: true, // allows web pack to insert new js and css without reload
+        port: 3000,
+        host: '0.0.0.0'//allows other devices on network to acess site
+    },
     mode: 'development',
-    watch: true, //makes it keep runing an watch for changes
+    //watch nolonger necessary after adding devServer
+    //watch: true, //makes it keep runing an watch for changes
     module: {
         rules: [ //tells web pack what to do with specific files
             {//test if the file name ends in .css
